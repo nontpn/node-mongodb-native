4005b8ad897a27f5bf3c744a92a6937501eb90b3
Matt Broadstone <mbroadst@mongodb.com>
2020-11-04 08:35:33 -0500

    temporarily use lodash for deep clone on command monitoring events

--- src/cmap/events.ts
+++ src/cmap/events.ts
@@ -4,6 +4,7 @@ import type { ConnectionPool, ConnectionPoolOptions } from './connection_pool';
 import type { Connection } from './connection';
 import type { Document } from '../bson';
 import type { AnyError } from '../error';
+import { cloneDeep } from 'lodash';
 
 /**
  * The base export class for all monitoring events published from the connection pool
@@ -395,7 +396,7 @@ function extractCommand(command: WriteProtocolMessageType): Document {
 
 function extractReply(command: WriteProtocolMessageType, reply?: Document) {
   if (reply) {
-    reply = JSON.parse(JSON.stringify(reply));
+    reply = cloneDeep(reply);
   }
 
   if (command instanceof KillCursor) {
